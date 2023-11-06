3fecf5d78f3c23579f3b3379efc436005c475682
Ayrat Badykov <ayratin555@gmail.com>
2023-11-06 21:48:09 +0200

    release 0.0.2 (#6)

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.0.2 (2023-11-06)
+
+### Enhancements
+- Listen to prompts from commands ([#5](https://github.com/ayrat555/cargo-mode/pull/5))
--- cargo-mode.el
+++ cargo-mode.el
@@ -24,7 +24,7 @@
 
 ;; Author: Ayrat Badykov <ayratin555@gmail.com>
 ;; URL: https://github.com/ayrat555/cargo-mode
-;; Version  : 0.0.1
+;; Version  : 0.0.2
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "25.1"))
 
